75ae37aaeb5eddbbc67ac11a4d21ba575d09629e
JT9 <caleb.tsosie@asu.edu>
2017-10-05 01:26:28 -0700

    Added setupRecorder func and record/stop button
    Added  a new func called setupRecorder that does the setup to enable recording of audio.  Also added functionality to the recordTapped button when the use taps it

--- SoundBoard/SoundViewController.swift
+++ SoundBoard/SoundViewController.swift
@@ -7,23 +7,63 @@
 //
 
 import UIKit
+//Used for audio
+import AVFoundation
 
 class SoundViewController: UIViewController {
-
+    
     
     @IBOutlet weak var recordButton: UIButton!
-    
-    
     @IBOutlet weak var soundNameTextField: UITextField!
     
     
-
+    var audioRecorder: AVAudioRecorder?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupRecorder()
     }
-
+    
+    func setupRecorder() {
+        
+        do {
+            //Create an audio session
+            let session = AVAudioSession.sharedInstance()
+            //Want to play audio and record
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            //Play from speaker
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            
+            //Create URL for the audio file
+            let basePath : String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            
+            let pathComponents = [basePath, "audio.mp4a"]
+            
+            let audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
+            print(audioURL)
+            
+            //Create settings for the audio recorder
+            
+            var settings : [String : AnyObject] = [:]
+            settings[AVFormatIDKey] = NSNumber(value: kAudioFormatMPEG4AAC)
+            settings[AVSampleRateKey] = NSNumber(value: 44100.0)
+            settings[AVNumberOfChannelsKey] = NSNumber(value: 2)
+        
+            
+            //Create AudioRecorder object
+            audioRecorder = try AVAudioRecorder(url: audioURL!, settings: settings)
+            audioRecorder!.prepareToRecord()
+            
+        } catch let error as NSError {
+            print(error)
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -32,11 +72,25 @@ class SoundViewController: UIViewController {
     
     @IBAction func recordTapped(_ sender: Any) {
         
+        if audioRecorder!.isRecording {
+            //Stop the recording
+            audioRecorder?.stop()
+            
+            //Change button title to Record
+            recordButton.setTitle("Record", for: .normal)
+        } else {
+            //Start the recording
+            audioRecorder?.record()
+            
+            //Change button title to Stop
+            recordButton.setTitle("Stop", for: .normal)
+            
+        }
     }
     @IBAction func playTapped(_ sender: Any) {
         
     }
-
+    
     @IBAction func addTapped(_ sender: Any) {
         
     }
