cfbaf0e7cbfd1b7ea0c4026238ac6f6e6bfed896
JT9 <caleb.tsosie@asu.edu>
2017-10-05 01:52:58 -0700

    Setting Up TableView for Sounds
    Setting up the TableView with needed funcs to list the recorded sounds

--- SoundBoard/ViewController.swift
+++ SoundBoard/ViewController.swift
@@ -8,12 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var sounds : [Sound] = []
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sounds.count
+    }
+    
+    //Sounds to get out of CoreData into the array
+    override func viewWillAppear(_ animated: Bool) {
+        sounds = 
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        let sound = sounds[indexPath.row]
+        cell.textLabel?.text = sound.name
+        
+        return cell
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
