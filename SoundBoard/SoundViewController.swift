e9cc1a1ec536701b5756b7b81d08e007693e637d
JT9 <caleb.tsosie@asu.edu>
2017-10-05 00:35:55 -0700

    Setup ViewControllers with buttons, nav bar, textField
    Added a tableView, new ViewController, buttons and a textField

---
+++ SoundBoard/SoundViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SoundViewController.swift
+//  SoundBoard
+//
+//  Created by C T on 10/5/17.
+//  Copyright © 2017 ASU. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    
+    @IBOutlet weak var soundNameTextField: UITextField!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func recordTapped(_ sender: Any) {
+        
+    }
+    @IBAction func playTapped(_ sender: Any) {
+        
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        
+    }
+    
+}
